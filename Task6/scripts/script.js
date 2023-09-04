1f1666659e6bb044254d4bee322ae53a2dadb724
Dilton D'souza <dilton.d'souza@zeuslearning.mumbai>
2023-09-04 03:55:56 -0400

    changes

--- Task6/scripts/script.js
+++ Task6/scripts/script.js
@@ -1,60 +1,34 @@
-
-
 const alerts_icon = document.getElementById('alerts-icon');
-
 const alerts_menu = document.getElementById('alerts-menu');
 
-// ✅ Show hidden DIV on hover
 alerts_icon.addEventListener('mouseover', function handleMouseOver() {
   alerts_menu.style.visibility = 'visible';
-
-  // 👇️ if you used visibility property to hide the div
-  // hiddenDiv.style.visibility = 'visible';
 });
 
-// ✅ (optionally) Hide DIV on mouse out
 alerts_icon.addEventListener('mouseout', function handleMouseOut() {
   alerts_menu.style.visibility = 'hidden';
-
-  // 👇️ if you used visibility property to hide the div
-  // hiddenDiv.style.visibility = 'hidden';
 });
+
 
 const announcements_icon = document.getElementById('announcements-icon');
-
 const announcements_menu = document.getElementById('announcements-menu');
 
-// ✅ Show hidden DIV on hover
 announcements_icon.addEventListener('mouseover', function handleMouseOver() {
-  announcements_menu.style.display = 'block';
-
-  // 👇️ if you used visibility property to hide the div
-  // hiddenDiv.style.visibility = 'visible';
+  announcements_menu.style.visibility = 'visible';
 });
 
-// ✅ (optionally) Hide DIV on mouse out
 announcements_icon.addEventListener('mouseout', function handleMouseOut() {
-  announcements_menu.style.display = 'none';
-
-  // 👇️ if you used visibility property to hide the div
-  // hiddenDiv.style.visibility = 'hidden';
+  announcements_menu.style.visibility = 'hidden';
 });
+
+
 const ham_icon = document.getElementById('hamburger-icon');
-
 const ham_menu = document.getElementById('hamburger-menu');
-
-// ✅ Show hidden DIV on hover
 ham_icon.addEventListener('mouseover', function handleMouseOver() {
-  ham_menu.style.display = 'block';
-
-  // 👇️ if you used visibility property to hide the div
-  // hiddenDiv.style.visibility = 'visible';
+  ham_menu.style.visibility = 'visible';
 });
 
 // ✅ (optionally) Hide DIV on mouse out
 ham_icon.addEventListener('mouseout', function handleMouseOut() {
-  ham_menu.style.display = 'none';
-
-  // 👇️ if you used visibility property to hide the div
-  // hiddenDiv.style.visibility = 'hidden';
+  ham_menu.style.visibility = 'hidden';
 });
